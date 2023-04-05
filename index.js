2e49d9f9412ef7705853393b9d9df281d61163ff
boris0711 <saldurion666@gmail.com>
2023-04-05 10:36:56 +0200

    updated emojis

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let myEmojis = ["👨‍💻", "⛹🏼", " 🚴🏼‍♂️", "🥊","🏋️"]
+let myEmojis = []
 const emojiContainer = document.getElementById("emoji-container")
 const emojiInput = document.getElementById("emoji-input")
 const pushBtn = document.getElementById("push-btn")
